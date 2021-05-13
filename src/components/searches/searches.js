9aa774559da2a5603db9899bad5d9335f58c20da
uzoma <chidumaga@gmail.com>
2021-05-13 09:58:36 +0100

    optimizes filter with useCallback hook

--- src/components/searches/searches.js
+++ src/components/searches/searches.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import searchStyles from './search.module.scss'
 import { supabase } from '../../App'
+// import { timeAgo } from '../../lib/timeago'
 
 const Searches = ({ pageTabs }) => {
     
     const [ searches, setSearches ] = useState([])
-
-    useEffect(() => {
-        getSearches().catch(console.error);
-    });
     
-    const getSearches = async () => {
+    const getSearches = useCallback(async () => {
         let { data: searches, error } = await supabase
             .from('searches')
             .select('*')
@@ -18,7 +15,11 @@ const Searches = ({ pageTabs }) => {
             .order("id", { ascending: false });
             if (error) console.log("error", error);
             setSearches(searches);
-    }
+    }, [pageTabs.isTabOneActive])
+
+    useEffect(() => {
+        getSearches().catch(console.error);
+    }, [getSearches]);
 
     return (
         <div>
@@ -30,7 +31,8 @@ const Searches = ({ pageTabs }) => {
                             <div className={searchStyles.footer}>
                                 { isFound && <p className={searchStyles.found}>found :)</p>}
                                 {/* <p>{suggestionCount} suggestions</p> */}
-                                <p>searched {createdAt}</p>
+                                {/* <p>searched {createdAt}</p> */}
+                                { console.log(typeof(createdAt)) }
                             </div>
                         </div>
                 )
