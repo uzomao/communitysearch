45da52dfb6aa4c749ec5c4f6718868d293ee8568
uzoma <chidumaga@gmail.com>
2021-05-25 17:56:12 +0100

    updates previous commit

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Header from '../../components/header/header'
 import { getTime } from '../../lib/helpers'
 import searchStyle from './search.module.scss'
@@ -11,25 +11,25 @@ const Search = ({ location }) => {
 
     const searchId = useParams().id
 
-    const getSearch = async () => {
+    const getSearch = useCallback(async () => {
         let { data: searches, error } = await supabase
             .from('searches')
             .select('*')
             .eq('id', searchId)
             if (error) console.log("error", error);
-            return searches[0]
-    }
+            setSearch(searches[0])
+    }, [searchId])
 
-    const search = location.state ? location.state.search : getSearch()
+    const [ search, setSearch ] = useState(null)
     const [ successMsg, setSuccessMsg ] = useState(location.state ? location.state.successMsg : null)
 
     useEffect(() => {
+        location.state ? setSearch(location.state.search) : getSearch()
         if(successMsg){
             window.setTimeout(() => setSuccessMsg(null), 3000)
         }
-    })
+    }, [ location.state, getSearch, successMsg])
 
-    const { id, title, description, createdAt } = search
 
     return (
         <div className={searchStyle.content}>
@@ -38,11 +38,11 @@ const Search = ({ location }) => {
             {
                 search &&
                     <div className={searchStyle.main}>
-                        <h2>{title}</h2>
+                        <h2>{search.title}</h2>
                         { successMsg && <p className="mt mb success-msg">{successMsg}</p>}
-                        <p>{description}</p>
+                        <p>{search.description}</p>
                         <div className={searchStyle.footer}>
-                            <p className={searchStyle.date}>Searched {getTime(createdAt)}</p>
+                            <p className={searchStyle.date}>Searched {getTime(search.createdAt)}</p>
                             <div>
                                 <Link to="#suggestion-box">
                                     <Buttons btnText="suggest something" />
@@ -53,7 +53,7 @@ const Search = ({ location }) => {
                     </div>
             }
 
-            <Suggestions searchId={id} />
+            <Suggestions searchId={searchId} />
         </div>
     )
 }
