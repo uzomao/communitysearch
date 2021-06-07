6f28dae2eef3a9c0fee87020d94d41223c31fdd1
uzoma <chidumaga@gmail.com>
2021-06-07 17:44:32 +0100

    adds menu overlay

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import headerStyles from './header.module.scss'
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo.png'
+import Menu from '../../components/menu/menu'
 
 const Header = () => {
+
+    const [ showMenu, setShowMenu ] = useState(false)
+
     return (
+        <>
         <header className={headerStyles.header}>
             <h2>
                 <Link to='/' className="default-link">community.search</Link>
@@ -17,10 +22,16 @@ const Header = () => {
                 <img 
                     className={headerStyles.logo} 
                     src={logo} 
-                    alt="community search logo, a large circle surrounded by five smaller circles" 
+                    alt="community search logo, a large circle surrounded by five smaller circles"
+                    onClick={() => setShowMenu(true)}
                 />
             </div>
         </header>
+        {
+            showMenu &&
+                <Menu setShowMenu={setShowMenu} />
+        }
+        </>
     )
 }
 
---
+++ src/components/menu/menu.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+import menuStyles from './menu.module.scss'
+import { Link } from 'react-router-dom'
+import Context from '../../context'
+import { MdClose } from "react-icons/md";
+
+const Menu = ({ setShowMenu }) => {
+
+    const { currentUser } = useContext(Context).value
+
+    return (
+       <main className={menuStyles.menu}>
+           <button className="text-btn-regular" onClick={() => setShowMenu(false)}>
+                <MdClose className={`close-btn ${menuStyles.close}`} />
+            </button>
+
+           <Link to='/' className={`default-link ${menuStyles.link} ${menuStyles.center}`}>
+               index
+           </Link>
+
+           <Link to='/about' id={menuStyles.about} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               about
+           </Link>
+
+           <Link to={`/profile/${currentUser.id}`} id={menuStyles.profile} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               profile
+           </Link>
+
+           <Link to='/community' id={menuStyles.community} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               community
+           </Link>
+
+           <Link to='/settings' id={menuStyles.settings} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               settings
+           </Link>
+
+           <Link to='/invite' id={menuStyles.invite} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               invite
+           </Link>
+
+           <Link to='/search' id={menuStyles.search} className={`default-link ${menuStyles.link} ${menuStyles.surrounding}`}>
+               search
+           </Link>
+       </main>
+    )
+}
+
+export default Menu
