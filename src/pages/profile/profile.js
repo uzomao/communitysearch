242a4a9f61d7741b0a5a4bf21fc65a4a6aac9a0e
uzoma <chidumaga@gmail.com>
2021-06-08 18:18:34 +0100

    implements add to community functionality

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useContext } from 'react'
 import Header from '../../components/header/header'
 import Tabs from '../../components/tabs/tabs'
 import Searches from '../../components/searches/searches'
@@ -7,7 +7,7 @@ import { useParams, Link } from 'react-router-dom'
 import profileStyles from './profile.module.scss'
 import { supabase } from '../../App'
 import Buttons from '../../lib/buttons'
-// import Context from '../../context'
+import Context from '../../context'
 
 const Profile = ({ page }) => {
 
@@ -19,8 +19,9 @@ const Profile = ({ page }) => {
     const [ profile, setProfile ] = useState(null)
 
     const [ successMsg, setSuccessMsg ] = useState(null)
+    const [ errorMsg, setErrorMsg ] = useState(null)
 
-    // const { currentUser } = useContext(Context).value
+    const { currentUser } = useContext(Context).value
 
     const getProfile = useCallback(async() => {
         let { data: person, error } = await supabase
@@ -31,38 +32,41 @@ const Profile = ({ page }) => {
             setProfile(person[0])
     }, [profileName])
 
-    // const addToCommunity = useCallback(async(name) => {
-    //     let community = []
-    //     const { data: person, error } = await supabase
-    //         .from('person')
-    //         .select('*')
-    //         .eq('id', currentUser.id)
-    //         community = person[0].community
+    const addToCommunity = useCallback(async(name) => {
 
-    //         if(community){
-    //             console.log(community)
-    //             if(!community.includes(name)){
-                    
-    //             }
-    //         }
-    //         // .update([
-    //         //     { community: [...community, name] },
-    //         // ])
-    //         // if (error) console.log("error", error);
-    //         // else {
-    //         //     console.log(person)
-    //         //     setSuccessMsg(`${name} has been added to your community`)
-    //         // }
-    // }, [profile, currentUser])
+        const { data: person, error } = await supabase
+            .from('person')
+            .select('*')
+            .eq('id', currentUser.id)
+            const community = person[0].community || []
+
+            if(!community.includes(name)){
+                const { error } = await supabase
+                    .from('person')
+                    .update([
+                        { community: [...community, name] },
+                    ])
+                    .eq('id', currentUser.id)
+                    if (error) console.log("error", error);
+                    else {
+                        setSuccessMsg(`${name} has been added to your community :)`)
+                    }
+            } else {
+                setErrorMsg(`${name} is already in your community`)
+            }
+            if(error) console.log("error", error)
+    }, [currentUser])
 
     useEffect(() => {
         getProfile().catch(console.error);
 
         if(successMsg){
             window.setTimeout(() => setSuccessMsg(null), 3000)
+        } else if(errorMsg){
+            window.setTimeout(() => setErrorMsg(null), 3000)
         }
 
-    }, [getProfile, successMsg]);
+    }, [getProfile, successMsg, errorMsg]);
 
     return (
         profile &&
@@ -70,6 +74,7 @@ const Profile = ({ page }) => {
                 <Header />
 
                 { successMsg && <p className="mt mb success-msg">{successMsg}</p>}
+                { errorMsg && <p className="mt mb error">{errorMsg}</p>}
 
                 <div className={profileStyles.header}>
                     { profile &&
@@ -79,7 +84,7 @@ const Profile = ({ page }) => {
                         </div>
                     }
                     <div className={profileStyles.buttons}>
-                        <Buttons btnText="add to your community" /*onClick={() => addToCommunity(profileName)}*/ />
+                        <Buttons btnText="add to your community" onClick={() => addToCommunity(profileName)} />
                         <Link to={`/profile/${profileName}/community`}>
                             <Buttons btnText="view community" />
                         </Link>
